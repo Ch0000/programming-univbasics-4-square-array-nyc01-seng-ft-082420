630d0083a025a09e57dc73f9db41e8b70b9f1520
Christian P Andrews <cpandrews91@gmail.com>
2020-08-18 07:51:18 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,37 +1,20 @@
-arr[1..4]
-def square_array
-  counter = 0
-  while i < counter.length do #|i|
-  end
-  arr.each { |i| arr << i ** 2} #arr.each_with_object([]) { |i, arr| arr << i ** 2}
-end
-  counter += 1
+
+def square_array(array)
+  num = []
+  array.each { |i| num << i ** 2 }
+  num
 end
 
+my_arr = [1, 2, 3]
+p square_array(my_arr) 
 
-Square = %w(1 2 3)
-new_array = Array.new
-
-def square_array
-while Square.size.times do
-  {|x|.each (Square[x] ** 2)}
-  p new_array
+def square_array(array)
+  num = []
+  array.each { |i| num << i ** 2 }
+  num
 end
-square_array
 
-my_arr = %w[9 10 16 25]
-#p square_array
-def square_array
-  index = 0
-while index < my_arr.length do
-puts "#{my_arr.collect {|x| x ** 2\n\n}}"
-end
-square_array
+new_numbers = [9, 10, 16, 25]
+p square_array(new_numbers)
 
 
-
-
-
-
-
-#{my_arr.collect {|x| x ** 2\n\n}}
